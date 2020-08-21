1452338435cbe333b1f5a9572869370e84909aff
binding-proc-8938 <naomirhames2020@gmail.com>
2020-08-21 05:13:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  family=[1,2,3]
+  []= array
 
 def retrieve_first_element_from_array(array)
   
